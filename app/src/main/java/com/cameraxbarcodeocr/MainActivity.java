b4ad9300c3676485a9f8d8fd0d400e46520f96e0
kaanozkaya <kaan@ozkaya.me>
2020-09-25 17:06:18 +0300

    Updated libs

--- app/src/main/java/com/cameraxbarcodeocr/MainActivity.java
+++ app/src/main/java/com/cameraxbarcodeocr/MainActivity.java
@@ -12,7 +12,7 @@ import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.DisplayMetrics;
 import android.util.Size;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -27,22 +27,20 @@ import com.cameraxbarcodeocr.mlkit.barcode.BarcodeAnalyzer;
 import com.cameraxbarcodeocr.mlkit.ocr.OcrAnalyzer;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.mlkit.vision.barcode.Barcode;
-import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.FocusMeteringAction;
-import androidx.camera.core.FocusMeteringResult;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.MeteringPoint;
@@ -55,7 +53,6 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity implements ImageAnalyzerInterface, MlKitInterface {
-   private static final String TAG = "MainActivity";
    private PreviewView previewView;
    private final int REQUEST_CODE = 111;
    private Camera camera;
@@ -64,11 +61,10 @@ public class MainActivity extends AppCompatActivity implements ImageAnalyzerInte
    private OcrAnalyzer ocrAnalyzer;
    private BarcodeAnalyzer barcodeAnalyzer;
    private boolean isAnalysingBarcode = true, isDoingOcr = false;
-   private long frameCount = 0;
    private SurfaceHolder transparentHolder;
+   private ImageView imageViewPreview;
    private float scaleX;
    private float scaleY;
-   private int screenWidth, previewWidth, previewHeight, screenHeight;
 
    @SuppressLint("ClickableViewAccessibility")
    @Override
@@ -77,34 +73,59 @@ public class MainActivity extends AppCompatActivity implements ImageAnalyzerInte
       setContentView(R.layout.activity_main);
 
       previewView = findViewById(R.id.previewView);
+      imageViewPreview = findViewById(R.id.imageViewOcrPreview);
 
       SurfaceView transparentSurface = findViewById(R.id.surfaceViewTransparent);
+      // Blocks camera preview if not on top
       transparentSurface.setZOrderOnTop(true);
       transparentHolder = transparentSurface.getHolder();
+      // Required to make surfaceView transparent
       transparentHolder.setFormat(PixelFormat.TRANSPARENT);
-
-      if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-         setCameraProviderListener();
-      else
-         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);
 
       ocrAnalyzer = new OcrAnalyzer(this);
       barcodeAnalyzer = new BarcodeAnalyzer(this);
 
-      previewView.setOnTouchListener(getTapToFocusOnTouchListener());
+      previewView.setOnTouchListener((View v, MotionEvent event) -> {
+         switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+               v.performClick();
+               return true;
+            case MotionEvent.ACTION_UP:
+               focus(event.getX(), event.getY());
+               return true;
+            default:
+               return false;
+         }
+      });
 
       findViewById(R.id.buttonOcr).setOnClickListener(view -> {
          isAnalysingBarcode = false;
+         isDoingOcr = true;
          imageAnalyzer.isAnalysing = true;
-         focus(screenWidth / 2f, screenHeight / 2f).addListener(() ->
-                         isDoingOcr = true
-                 , Executors.newSingleThreadExecutor());
+
+         Bitmap bitmap = previewView.getBitmap();
+         imageViewPreview.setImageBitmap(bitmap);
+         imageViewPreview.setVisibility(View.VISIBLE);
       });
 
       findViewById(R.id.buttonBarcode).setOnClickListener(view -> {
          isAnalysingBarcode = true;
          imageAnalyzer.isAnalysing = true;
       });
+      findViewById(R.id.buttonClearOcr).setOnClickListener(view -> {
+         clearCanvas();
+         imageViewPreview.setVisibility(View.GONE);
+      });
+
+   }
+
+   @Override
+   protected void onResume() {
+      super.onResume();
+      if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+         setCameraProviderListener();
+      else
+         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);
    }
 
    @SuppressLint("UnsafeExperimentalUsageError")
@@ -112,19 +133,11 @@ public class MainActivity extends AppCompatActivity implements ImageAnalyzerInte
    public void onImageAnalyzed(ImageProxy imageProxy) {
       if (scaleX == 0)
          calculateScales();
-      if (isAnalysingBarcode) {
-         if (frameCount++ % 1 != 0) {
-            imageProxy.close();
-            return;
-         }
+      if (isAnalysingBarcode)
          barcodeAnalyzer.analyzeBarcode(imageProxy);
-      } else if (isDoingOcr) {
+      else if (isDoingOcr) {
          imageAnalyzer.isAnalysing = false;
          isDoingOcr = false;
-         Bitmap bitmap = InputImage.fromMediaImage(imageProxy.getImage(),
-                 imageProxy.getImageInfo().getRotationDegrees()).getBitmapInternal();
-
-         runOnUiThread(() -> ((ImageView) findViewById(R.id.imageViewOcrPreview)).setImageBitmap(bitmap));
          ocrAnalyzer.extractText(imageProxy);
       }
    }
@@ -135,10 +148,7 @@ public class MainActivity extends AppCompatActivity implements ImageAnalyzerInte
          clearCanvas();
          if (barcode.getBoundingBox().height() <= 35)
             continue;
-         Log.d(TAG, "standart :"
-                 + barcode.getBoundingBox().top + " " + barcode.getBoundingBox().bottom + " "
-                 + barcode.getBoundingBox().left + " " + barcode.getBoundingBox().right);
-         drawRect(getScaledRect(barcode.getBoundingBox(), scaleX, scaleY));
+         drawRect(getScaledRect(barcode.getBoundingBox()));
          break;
       }
 
@@ -146,19 +156,39 @@ public class MainActivity extends AppCompatActivity implements ImageAnalyzerInte
 
    @Override
    public void onTextExtracted(Text firebaseVisionText) {
+      List<String> texts = new ArrayList<>();
+      List<RectF> boundingBoxes = new ArrayList<>();
+      for (Text.TextBlock textBlock : firebaseVisionText.getTextBlocks())
+         for (Text.Line line : textBlock.getLines())
+            for (Text.Element element : line.getElements()) {
+               texts.add(element.getText());
+               boundingBoxes.add(getScaledRect(element.getBoundingBox()));
+            }
 
+      drawTexts(texts, boundingBoxes);
    }
 
    private void drawRect(RectF rectangle) {
-      Log.d(TAG, "drawRect: " + rectangle.top + " " + rectangle.bottom + " " + rectangle.left + " " + rectangle.right);
       Canvas canvas = transparentHolder.lockCanvas();
-      if (canvas == null)
-         return;
+
       Paint myPaint = new Paint();
-      myPaint.setColor(Color.rgb(255, 200, 0));
-      myPaint.setStrokeWidth(10);
+      myPaint.setColor(Color.rgb(0, 255, 0));
+      myPaint.setStrokeWidth(5);
       myPaint.setStyle(Paint.Style.STROKE);
       canvas.drawRect(rectangle, myPaint);
+
+      transparentHolder.unlockCanvasAndPost(canvas);
+   }
+
+   private void drawTexts(List<String> texts, List<RectF> boundingBoxes) {
+      Canvas canvas = transparentHolder.lockCanvas();
+
+      Paint myPaint = new Paint();
+      myPaint.setColor(Color.rgb(255, 0, 0));
+      myPaint.setStyle(Paint.Style.FILL);
+      myPaint.setTextSize(convertDpToPixel(13));
+      for (int i = 0; i < texts.size(); i++)
+         canvas.drawText(texts.get(i), boundingBoxes.get(i).left, boundingBoxes.get(i).bottom, myPaint);
 
       transparentHolder.unlockCanvasAndPost(canvas);
    }
@@ -185,9 +215,9 @@ public class MainActivity extends AppCompatActivity implements ImageAnalyzerInte
       previewView.setImplementationMode(PreviewView.ImplementationMode.COMPATIBLE);
       preview = new Preview.Builder().setTargetResolution(new Size(1080, 1920)).build();
       CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
-      preview.setSurfaceProvider(previewView.createSurfaceProvider());
-      // Seting target AR doesn't give highest resolution sometimes
-      // Default backpressure strategy drops latest frames
+      preview.setSurfaceProvider(previewView.getSurfaceProvider());
+      // Setting target AR may not give highest resolution
+      // Default back pressure strategy drops latest frames
       ImageAnalysis imageAnalysis = new ImageAnalysis.Builder().setTargetResolution(new Size(1080, 1920)).build();
       ExecutorService executorService = Executors.newSingleThreadExecutor();
       imageAnalyzer = new MyImageAnalyzer(this);
@@ -199,25 +229,21 @@ public class MainActivity extends AppCompatActivity implements ImageAnalyzerInte
    @SuppressLint("RestrictedApi")
    private void calculateScales() {
       Size previewSize = preview.getAttachedSurfaceResolution();
-      previewWidth = previewSize.getHeight();// inverted because preview is rotated
-      previewHeight = previewSize.getWidth();
-      screenWidth = previewView.getWidth();
-      screenHeight = previewView.getHeight();
-      scaleX = previewWidth / (float) screenWidth;
-      scaleY = previewHeight / (float) screenHeight;
+      int previewWidth = previewSize.getHeight();// inverted because preview is rotated
+      int previewHeight = previewSize.getWidth();
+      int usableWidth = previewView.getWidth();
+      int usableHeight = previewView.getHeight();
+      scaleX = usableWidth / (float) previewWidth;
+      scaleY = usableHeight / (float) previewHeight;
       scaleX = new BigDecimal(scaleX).setScale(2, BigDecimal.ROUND_CEILING).floatValue();
       scaleY = new BigDecimal(scaleY).setScale(2, BigDecimal.ROUND_CEILING).floatValue();
    }
 
-   private RectF getScaledRect(Rect rect, float scaleX, float scaleY) {
+   private RectF getScaledRect(Rect rect) {
       RectF rectF = new RectF(rect);
       Matrix matrix = new Matrix();
-      matrix.postScale(1 / scaleX, 1 / scaleY);
+      matrix.postScale(scaleX, scaleY);
       matrix.mapRect(rectF);
-//      rect.right = (int) ((rect.right - previewWidth / 2) * scaleX + screenWidth / 2);
-//      rect.left = (int) ((rect.left - previewWidth / 2) * scaleX + screenWidth / 2);
-//      rect.top = (int) ((rect.top - previewHeight / 2) * scaleY + screenHeight / 2);
-//      rect.bottom = (int) ((rect.bottom - previewHeight / 2) * scaleY + screenHeight / 2);
       return rectF;
    }
 
@@ -229,30 +255,14 @@ public class MainActivity extends AppCompatActivity implements ImageAnalyzerInte
             setCameraProviderListener();
    }
 
-   protected ListenableFuture<FocusMeteringResult> focus(float x, float y) {
-      Log.d(TAG, "focus: tap to focus");
+   protected void focus(float x, float y) {
       MeteringPointFactory factory = new SurfaceOrientedMeteringPointFactory(1.0f, 1.0f);
       MeteringPoint point = factory.createPoint(x, y);
-      FocusMeteringAction action = new FocusMeteringAction.Builder(point, FocusMeteringAction.FLAG_AF)
-              .setAutoCancelDuration(5, TimeUnit.SECONDS)
-              .build();
-
-      return camera != null ? camera.getCameraControl().startFocusAndMetering(action) : null;
+      FocusMeteringAction action = new FocusMeteringAction.Builder(point, FocusMeteringAction.FLAG_AF).build();
+      camera.getCameraControl().startFocusAndMetering(action);
    }
 
-   public View.OnTouchListener getTapToFocusOnTouchListener() {
-      return (View v, MotionEvent event) -> {
-         switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-               v.performClick();
-               break;
-            case MotionEvent.ACTION_UP:
-               focus(event.getX(), event.getY());
-               return true;
-            default:
-               return false;
-         }
-         return true;
-      };
+   public float convertDpToPixel(float dp) {
+      return dp * ((float) getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
    }
 }
